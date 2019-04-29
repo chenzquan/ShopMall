24152faea1639aed69035a8b16e50e3b057801ac
chenzquan <122705737@qq.com>
2019-04-29 21:56:58 +0800

    lock

--- src/main/java/com/mmall/task/CloseOrderTask.java
+++ src/main/java/com/mmall/task/CloseOrderTask.java
@@ -19,7 +19,7 @@ public class CloseOrderTask {
     private IOrderService iOrderService;
 
     //这样写的话 集群下的后台 都会执行
-    @Scheduled(cron = "0 */1 * * * ?")//每1分钟(每个1分钟的整数倍)
+//    @Scheduled(cron = "0 */1 * * * ?")//每1分钟(每个1分钟的整数倍)
     public void closeOrderTaskV1(){
         log.info("关闭订单定时任务启动");
         int hour = Integer.parseInt(PropertiesUtil.getProperty("close.order.task.time.hour","2"));
@@ -27,7 +27,8 @@ public class CloseOrderTask {
         log.info("关闭订单定时任务结束");
     }
 
-
+    //这里设置的value是一个时间戳 但没有利用起来
+    @Scheduled(cron = "0 */1 * * * ?")
     public void closeOrderTaskV2(){
         log.info("关闭订单定时任务启动");
 
@@ -40,17 +41,20 @@ public class CloseOrderTask {
             closeOrder(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
         }else{
             log.info("没有获得分布式锁:{}",Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
-
         }
-
         log.info("关闭订单定时任务结束");
     }
+
+
+
+
+
 
     private void  closeOrder(String lockName){
         RedisPoolUtil.expire(lockName,50); //有效期为50秒， 防止死锁
         log.info("获取{}，ThreadName:{}",Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK,Thread.currentThread().getName());
         int hour = Integer.parseInt(PropertiesUtil.getProperty("closeo.order.task.time.hour","2"));
-        iOrderService.closeOrder(hour);
+//        iOrderService.closeOrder(hour);
         RedisPoolUtil.del(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
 
         log.info("释放{}，ThreadName:{}",Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK,Thread.currentThread().getName());
